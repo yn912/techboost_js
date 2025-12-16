5f44a557b98ff5cc91593f8c256173c2f67673aa
taro <taro@example.com>
2025-12-16 16:03:07 +0000

    テキスト６課題

---
+++ 6_07.js
@@ -0,0 +1,11 @@
+let fruit  = ['apricot','strawberry'];
+console.log(fruit);
+
+fruit.unshift('pear');
+console.log(fruit);
+
+fruit.push('grape');
+console.log(fruit);
+
+fruit[fruit.length - 1]='lime';
+console.log(fruit);
