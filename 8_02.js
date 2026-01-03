3351c319e541084c2fbd5feee80bf768d85fc685
taro <taro@example.com>
2026-01-03 16:51:21 +0000

    テキスト8の課題

---
+++ 8_02.js
@@ -0,0 +1,13 @@
+function getTriangleArea(base, height) {
+    return (base * height) / 2;
+  }
+
+  let area = getTriangleArea(8 , 6);
+console.log(area);
+
+var getTriangleArea = function(base , height){
+  let calc = base * height / 2;
+  return calc;
+}
+
+console.log(getTriangleArea(8 , 6));
